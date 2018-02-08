af23f3e3f1b7c9b56641415058ecf08dee87b150
Ahmed Ali <ahmed@ebi.ac.uk>
2018-02-08 13:59:15 +0000

    Settings

--- settings/settings_no_password.py
+++ settings/settings_no_password.py
@@ -1,7 +1,9 @@
 import os
+
 BASE_DIRECTORY = ''
 LOAD_DIR = ''
-SUBMISSION_TOOL_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'resources','BS_SubmissionTool.jar'))
+SUBMISSION_TOOL_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'resources', 'BS_SubmissionTool.jar'))
 ARRAY_DIR = ''
 EXPORTER_SCRIPT = ''
 
@@ -14,6 +16,5 @@ AE2 = {
     'port': '',
     'username': '',
     'password': '',
-
 
 }
